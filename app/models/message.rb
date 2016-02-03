5995d2ba117656e1551df5468a8a1c11afe76f1f
ymn <i0ub23x4k78t302p@icloud.com>
2016-02-03 08:24:29 +0000

    2.3,L6 section7.2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ActiveRecord::Base
+     # 名前は必須入力かつ20文字以内
+  validates :name , length: { maximum: 20 } , presence: true
+     # 内容は必須入力かつ２文字以上３０文字以内
+     # 値が存在すれば検証成功（presence: true）
+  validates :body , length: { minimum: 2, maximum: 30 } , presence: true     
 end
