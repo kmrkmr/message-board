cfaa30c6ee81b1c6d2fe91a0f950a53e9c1099d4
ymn <i0ub23x4k78t302p@icloud.com>
2016-02-07 08:43:50 +0000

    validates modify

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ActiveRecord::Base
+     # 年齢が３以上かどうか
+  validates :age , numericality: { greater_than_or_equal_to: 3 } , presence: true     
      # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
      # 内容は必須入力かつ２文字以上３０文字以内
